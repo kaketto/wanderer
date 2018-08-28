133d06c575aa0065c63e138acfbaf1bdc9a874f2
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-28 18:00:55 +0200

    feat:hero moves on the canvas by arrow keys

--- src/hero.ts
+++ src/hero.ts
@@ -1,15 +1,18 @@
 import { Character } from "./character";
 import { rollDice } from "./dice";
 
+
 'use strict';
 
 export class Hero extends Character {
+  
   constructor(name: string = 'Hero', level: number = 1, currentPos: number[] = [0, 0]) {
     super(name, level, currentPos);
     this.name = name;
     this.level = level;
     this.getStarted();
     this.currentPos = currentPos;
+    this.appear(currentPos[0], currentPos[1]);
   }
 
   strike(): void {
@@ -23,10 +26,55 @@ export class Hero extends Character {
     this.StrikePoint = 5 + rollDice();
   }
 
-  move(): void {
-    throw new Error("Method not implemented.");
+  appear(x: number, y: number) {
+    const image = document.getElementById('hero-down') as HTMLImageElement;
+    this.ctx.drawImage(image, x, y);
   }
 
+  moveRight(): void {
+    if (super.getCurrentPos()[0] === 648) {
+      const image = document.getElementById('hero-right') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1]);
+    } else {
+      const image = document.getElementById('hero-right') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0] + 72, this.currentPos[1]);
+      this.currentPos = [this.currentPos[0] + 72, this.currentPos[1]];
+    }
+  }
+
+  moveLeft(): void {
+    if (super.getCurrentPos()[0] === 0) {
+      const image = document.getElementById('hero-left') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1]);
+    } else { 
+      const image = document.getElementById('hero-left') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0] - 72, this.currentPos[1]);
+      this.currentPos = [this.currentPos[0] - 72, this.currentPos[1]];
+    }
+  }
+
+  moveUp(): void {
+    if (super.getCurrentPos()[1] === 0) {
+      const image = document.getElementById('hero-up') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1]);
+    } else { 
+    const image = document.getElementById('hero-up') as HTMLImageElement;
+    this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1] - 72);
+    this.currentPos = [this.currentPos[0], this.currentPos[1] - 72];
+    }
+  }
+
+  moveDown(): void {
+    if (super.getCurrentPos()[1] === 648) {
+      const image = document.getElementById('hero-down') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1]);
+    } else {  
+      const image = document.getElementById('hero-down') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1] + 72);
+      this.currentPos = [this.currentPos[0], this.currentPos[1] + 72];
+    }  
+  }
+  
   gameEnd(): void {
     if (super.isDead) {
       console.log(`GAME OVER`);
@@ -34,5 +82,3 @@ export class Hero extends Character {
   }
 }
 
-let me = new Hero();
-console.log(me);
