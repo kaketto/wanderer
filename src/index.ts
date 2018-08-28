faf73ce879d1a7141213d8d61c6bcace7d3470b5
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-28 18:02:45 +0200

    feat: arrow keys funcionality is working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
 import { Map } from "./map";
+import { Hero } from "./hero";
 
 'use strict';
 
 let level1: Map = new Map(1);
 level1.DrawAllTiles(0, 0);
 //drawWall();
+
+let you = new Hero();
+
+function onKeyPress(event: any) {
+  switch (event.keyCode) {
+    case 37:
+      level1.drawOneTile(you.getCurrentPos()[0], you.getCurrentPos()[1]);
+      you.moveLeft();
+      break;
+    case 38:
+      level1.drawOneTile(you.getCurrentPos()[0], you.getCurrentPos()[1]);
+      you.moveUp();
+      break;
+    case 39:
+      level1.drawOneTile(you.getCurrentPos()[0], you.getCurrentPos()[1]);
+      you.moveRight();
+      break;
+    case 40:
+      level1.drawOneTile(you.getCurrentPos()[0], you.getCurrentPos()[1]);
+      you.moveDown();
+      break;
+  }
+}
+
+document.body.addEventListener('keydown', onKeyPress);
