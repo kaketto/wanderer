a972b44f89b7490069ddd7cb5a8cbca131d20136
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-08-29 17:16:48 +0200

    add: monster class

---
+++ src/monster.ts
@@ -0,0 +1,55 @@
+'use strict';
+import { Character } from "./character";
+import { rollDice, randomPosition, fixedWall } from "./dice";
+
+export class Monster extends Character {
+  
+  constructor(name: string = 'Monster', level: number = 1, currentPos: number[] = [randomPosition()[0], randomPosition()[1]]) {
+    super(name, level, currentPos);
+    this.name = name;
+    this.level = level;
+    this.getStarted();
+    this.currentPos = currentPos;
+    this.appear();
+  }
+
+  strike(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  getStarted(): void {
+    this.maxHealthPoint = (rollDice() + rollDice()) * this.level;
+    this.currentHealthPoint = this.maxHealthPoint;
+    this.DefendPoint = rollDice() * this.level / 2;
+    this.StrikePoint = rollDice() * this.level;
+  }
+
+  appear() {
+    if (fixedWall.some((elem) => elem[0] === this.currentPos[0] && elem[1] === this.currentPos[1])) {
+      this.appear();
+    } else {
+      const image = document.getElementById('skeleton') as HTMLImageElement;
+      this.ctx.drawImage(image, this.currentPos[0], this.currentPos[1]);
+    }  
+  }
+
+  move() {
+    let possibleMove: number[][] = [[this.currentPos[0] + 72, this.currentPos[1]], [this.currentPos[0] - 72, this.currentPos[1]], 
+      [this.currentPos[0], this.currentPos[1] + 72], [this.currentPos[0], this.currentPos[1] - 72]];
+    let randomMove: number[] = possibleMove[Math.floor(Math.random() * 4)];
+    if (fixedWall.some((elem) => elem[0] === randomMove[0] && elem[1] === randomMove[1]) || randomMove[0] === 720 || randomMove[0] === -72 || 
+      randomMove[1] === 720 || randomMove[1] === -72) {
+      this.move();
+    } else {
+      const image = document.getElementById('skeleton') as HTMLImageElement;
+      this.ctx.drawImage(image, randomMove[0], randomMove[1]);
+      this.currentPos = randomMove;
+    }
+  }
+  
+  gameEnd(): void {
+    if (super.isDead) {
+      console.log(`Well done, keep going`);
+    }
+  }
+}
